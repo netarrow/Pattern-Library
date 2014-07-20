00fbe757c78a3f8dd6e576155e71baf0767de7f6
netarrow <netarrow89@hotmail.com>
2014-07-20 11:50:17 +0200

    iterator

---
+++ src/Behavioral/IteratorTest.java
@@ -0,0 +1,11 @@
+package Behavioral;
+
+/**
+ * Provide a way to access the elements of an aggregate object sequentially without exposing its underlying representation.
+ */
+
+// already well provided by all frameworks
+
+public class IteratorTest {
+
+}
